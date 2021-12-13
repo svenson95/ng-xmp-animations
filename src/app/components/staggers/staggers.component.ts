93f8481fcdecb7bdbb508f15c8d70c54c2084871
Sven Brodny <sven.brodny@piobyte.de>
2021-12-13 18:03:25 +0100

    refactor: staggers component * removed scroll event / in viewport property in stagger example

--- src/app/components/staggers/staggers.component.ts
+++ src/app/components/staggers/staggers.component.ts
@@ -1,141 +1,52 @@
-import {
-  Component,
-  ElementRef,
-  NgZone,
-  OnDestroy,
-  OnInit,
-  ViewChild,
-} from '@angular/core';
-import {
-  animate,
-  query,
-  stagger,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
+import {Component, ElementRef, ViewChild,} from '@angular/core';
+import {animate, keyframes, query, stagger, style, transition, trigger,} from '@angular/animations';
 
-import { ScrollService } from '../../services/scroll.service';
-import { Subscription } from 'rxjs';
-
-const MARGIN = '800px';
+const LOREMIPSUM = [
+  '@angular/animations is installed by default',
+  'Animation provides the illusion of motion: HTML elements change styling over time',
+  'Well-designed animations can make your application more fun and straightforward to use',
+  'Motion greatly enhances the user experience',
+  "Angular's animation system is built on CSS functionality",
+]
 
 @Component({
   selector: 'app-staggers',
   templateUrl: './staggers.component.html',
   styleUrls: ['./staggers.component.scss'],
   animations: [
-    trigger('stagger', [
-      state(
-        'hidden',
-        style({
-          // initial
-          height: '*',
-          transform: `translateX(-${MARGIN})`,
-        })
-      ),
-      state(
-        'visible',
-        style({
-          // initial
-          height: '*',
-        })
-      ),
-      transition('hidden => visible', [
-        query(
-          '.card',
-          stagger('150ms ease-in-out', [
-            style({
-              // initial
-              opacity: 0,
-              height: '0px',
-              'padding-top': '0',
-              'padding-bottom': '0',
-            }),
-            animate(
-              '250ms ease-in-out',
-              style({
-                // final
-                opacity: 1,
-                transform: `translateX(${MARGIN})`,
-                height: '*',
-                'padding-top': '*',
-                'padding-bottom': '*',
-              })
-            ),
-          ])
-        ),
-      ]),
-      transition('visible => hidden', [
-        query(
-          '.card',
-          stagger('120ms ease-in-out', [
-            style({
-              // initial
-              height: '*',
-              'padding-top': '*',
-              'padding-bottom': '*',
-              opacity: 1,
-            }),
-            animate(
-              '250ms ease-in-out',
-              style({
-                // final
-                height: '0px',
-                'padding-top': '0',
-                'padding-bottom': '0',
-                opacity: 0,
-                transform: `translateX(-${MARGIN})`,
-              })
-            ),
-          ])
-        ),
-      ]),
+    trigger('staggerAnimation', [
+      transition('* => *', [
+        query(':enter', style({opacity: 0}), {optional: true}),
+        query(':enter', stagger('200ms', [
+          animate('400ms ease-in', keyframes([
+            style({opacity: 0, transform: 'translateX(-75px)', offset: 0}),
+            style({opacity: .5, transform: 'translateX(35px)', offset: 0.3}),
+            style({opacity: 1, transform: 'translateX(0)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+
+        query(':leave', stagger('100ms', [
+          animate('300ms ease-in', keyframes([
+            style({opacity: 1, transform: 'translateX(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateX(35px)', offset: 0.3}),
+            style({opacity: 0, transform: 'translateX(-75px)', offset: 1}),
+          ]))
+        ]), {optional: true}),
+      ])
     ]),
   ],
 })
-export class StaggersComponent implements OnInit, OnDestroy {
-  inViewport = false;
-  cards = [
-    '@angular/animations is installed by default',
-    'Animation provides the illusion of motion: HTML elements change styling over time',
-    'Well-designed animations can make your application more fun and straightforward to use',
-    'Motion greatly enhances the user experience',
-    "Angular's animation system is built on CSS functionality",
-  ];
-  scrolling$!: Subscription;
+export class StaggersComponent {
+  @ViewChild('cardsContainer') public cardsContainer!: ElementRef;
+  public array: string[] = [];
+  public showStagger: boolean = false;
 
-  get staggerState() {
-    return this.inViewport ? 'visible' : 'hidden';
-  }
-
-  @ViewChild('cardsContainer') cardsContainer!: ElementRef;
-
-  constructor(private scroll: ScrollService, private ngZone: NgZone) {}
-
-  ngOnInit() {
-    this.ngZone.runOutsideAngular(() => {
-      this.scrolling$ = this.scroll.scrollEvent$.subscribe((_) => {
-        this.ngZone.run(() => {
-          this.inViewport = this.isInViewport(
-            this.cardsContainer.nativeElement
-          );
-        });
-      });
-    });
-  }
-
-  isInViewport(el: HTMLElement) {
-    const { top } = el.getBoundingClientRect();
-    return top <= 700;
-  }
-
-  toggleSlide() {
-    this.inViewport = !this.inViewport;
-  }
-
-  ngOnDestroy() {
-    this.scrolling$.unsubscribe();
+  public toggleSlide(): void {
+    if (this.showStagger) {
+      this.array = [];
+    } else {
+      this.array = LOREMIPSUM;
+    }
+    this.showStagger = !this.showStagger;
   }
 }
