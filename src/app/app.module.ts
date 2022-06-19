4b1cc09b2740ae1e2a4bb26a3ccbd47c400f3fa3
Sven Brodny <svenbrodny@googlemail.com>
2022-06-19 18:11:01 +0200

    refactor: core components (header footer)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-import { EnterComponent } from "./components/enter/enter.component";
+import { EnterComponent } from './components/enter/enter.component';
 import { ScrollPointComponent } from './components/scroll-point/scroll-point.component';
 import { StaggersComponent } from './components/staggers/staggers.component';
-import { ScrollService } from "./services/scroll.service";
+import { FooterComponent } from './core/footer/footer.component';
+import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, EnterComponent, ScrollPointComponent, StaggersComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
+  declarations: [
+    AppComponent,
+    EnterComponent,
+    ScrollPointComponent,
+    StaggersComponent,
+    FooterComponent,
+    HeaderComponent,
   ],
-  providers: [ScrollService],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, BrowserAnimationsModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
