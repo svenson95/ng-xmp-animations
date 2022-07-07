ad28bfa4d185f0458292bd5d34f38892797486c0
Sven Brodny <svenbrodny@googlemail.com>
2022-07-07 13:16:16 +0200

    Removed old comment

--- src/app/services/scroll.service.ts
+++ src/app/services/scroll.service.ts
@@ -1,13 +1,10 @@
 import { Injectable } from '@angular/core';
-import { fromEvent } from "rxjs";
-import { debounceTime } from "rxjs/operators";
+import { fromEvent } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ScrollService {
   scrollEvent$ = fromEvent(window, 'scroll').pipe(debounceTime(30));
-  // Event "wheel" works only if user uses mouse wheel to scroll
-  // but user can scroll by pressing "space" or drag scrollbar by mouse
-  // End in this case scrolling won't be fired
 }
