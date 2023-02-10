0f28dddb9f4fce34ceae3a4af8c4c6774fb7645e
Sven Brodny <svenbrodny@googlemail.com>
2023-02-10 18:29:11 +0100

    refac header & footer with ng-xmp-base lib

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { NgXmpBaseModule } from "ng-xmp-base";
+
 import { AppComponent } from './app.component';
 
 import { EnterComponent } from './components/enter/enter.component';
 import { ScrollPointComponent } from './components/scroll-point/scroll-point.component';
 import { StaggersComponent } from './components/staggers/staggers.component';
-import { FooterComponent } from './core/footer/footer.component';
-import { HeaderComponent } from './core/header/header.component';
 
 @NgModule({
   declarations: [
@@ -16,10 +16,8 @@ import { HeaderComponent } from './core/header/header.component';
     EnterComponent,
     ScrollPointComponent,
     StaggersComponent,
-    FooterComponent,
-    HeaderComponent,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, NgXmpBaseModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
